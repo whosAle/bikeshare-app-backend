847c13f389774d8e2f844176c49cfbb5fc162bbb
whosAle <abelgrave95@gmail.com>
2019-05-24 11:04:06 -0400

    add controller actions for bike networks

--- app/controllers/bike_networks_controller.rb
+++ app/controllers/bike_networks_controller.rb
@@ -1,2 +1,31 @@
 class BikeNetworksController < ApplicationController
+
+  def new
+    bike_network = BikeNetwork.new
+
+    render json: bike_network
+  end
+
+  def show
+    bike_network = BikeNetwork.find(params[:id])
+
+    render json: bike_network
+  end
+
+  def create
+    bike_network = BikeNetwork.create(bike_network_params)
+
+    if !bike_network.valid?
+      bike_network = bike_network.errors.full_messages
+    end
+
+    render json: bike_network
+  end
+
+  private
+
+  def bike_network_params
+    params.require(:bike_network).permit(:name)
+  end
+
 end
